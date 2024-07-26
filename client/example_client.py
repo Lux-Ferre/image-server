29a00627f3fa116422c0acd4a6e94834a796b8d3
Lux-Ferre <Lux-Ferre@users.noreply.github.com>
2024-07-26 15:09:12 +0100

    example client fully featured
    Image taken as a command line argument, allowing it to be called from snipping tools (eg Greenshot)
    
    Response url is copied to clipboard, and displayed in a notification.

--- client/example_client.py
+++ client/example_client.py
@@ -1,13 +1,35 @@
+import sys
 import requests
+import pyperclip
+from plyer import notification
 
 
-url = "http://127.0.0.1:5000/upload"
-file_path = "image.png"
+if len(sys.argv) != 2:
+	print("Usage: python example_client.py <file_path>")
+	sys.exit(1)
+
+file_path = sys.argv[1]
+
+url = "https://img.luxferre.dev/upload"
 
 headers = {'X-API-KEY': "secret_key"}
 
 with open(file_path, "rb") as file:
 	files = {"file": file}
 	response = requests.post(url, files=files, headers=headers)
-	print(response.json())
 
+if response.status_code == 201:
+	response_json = response.json()
+	upload_url = response_json.get('url', 'No URL in response')
+	notification_text = f"Copied to clipboard.\n\n {upload_url}"
+	notification_title = "Upload successful"
+	pyperclip.copy(upload_url)
+else:
+	notification_text = "Unknown error"
+	notification_title = "Upload Unsuccessful"
+
+notification.notify(
+    title=notification_title,
+    message=notification_text,
+    timeout=10
+)
