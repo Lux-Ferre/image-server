a87c8c0745b2ee6e2c04064e50b34362b84ebbe8
Lux-Ferre <Lux-Ferre@users.noreply.github.com>
2024-07-30 15:47:08 +0100

    routes for new error pages

--- flask_app/views/errors.py
+++ flask_app/views/errors.py
@@ -2,6 +2,16 @@ from flask import render_template
 from flask_app import app
 
 
-@app.errorhandler(404)
+@app.errorhandler(401)
 def not_found(e):
+	return render_template("errors/401.html")
+
+
+@app.errorhandler(403)
+def unauthenticated(e):
+	return render_template("errors/403.html")
+
+
+@app.errorhandler(404)
+def forbidden(e):
 	return render_template("errors/404.html")
